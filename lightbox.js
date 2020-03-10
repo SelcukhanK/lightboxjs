7c9cb3d72f581bfd50f6d30e70062186409f58b8
SelcukhanK <56695065+SelcukhanK@users.noreply.github.com>
2020-03-10 12:43:36 +0100

    afgewerkt

--- lightbox.js
+++ lightbox.js
@@ -3,19 +3,34 @@ const alleDuimen     = document.querySelectorAll('.duim');
 
 let alleInhoud = [];
 
-for(let i=o; i<alleInformatie.length; i++) {
+for(let i=0; i<alleInformatie.length; i++) {
     alleInhoud.push(alleInformatie[i]);
     alleInformatie[i].remove();
 }
+
+function verwijderModaal() {
+    document.getElementById('modaal').remove();
+}
+
+
 
 function maakModaal(num) {
     console.log(alleInhoud[num]);
     let modaal = document.createElement('div');
     modaal.className = 'modaal';
-
+    modaal.id = 'modaal';
+    modaal.addEventListener('click', verwijderModaal);
+    // sluitknop maken
+    let sltknp = document.createElement('i');
+    sltknp.className = 'fas fa-times-circle sluit-knop';
+    sltknp.addEventListener('click', verwijderModaal);
     let modaalContainer = document.createElement('div');
     modaalContainer.className = 'modaal-container';
     modaalContainer.innerHTML = alleInhoud[num].innerHTML;
+    modaalContainer.addEventListener('click', function(e) {
+        e.stopPropagation();
+    });
+    modaalContainer.prepend(sltknp);
     modaal.append(modaalContainer);
     document.body.append(modaal);
 }
@@ -25,4 +40,3 @@ for(let i=0; i<alleDuimen.length; i++) {
         maakModaal(i)
     })
 }
-
